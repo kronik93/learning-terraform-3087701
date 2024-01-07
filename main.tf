e6d00bb244061864e7d83b7f42921ec85bf85463
Paul Khoury Hazouri <73915000+kronik93@users.noreply.github.com>
2024-01-07 18:58:52 +0100

    fix format

--- main.tf
+++ main.tf
@@ -19,9 +19,8 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
@@ -47,21 +46,19 @@ resource "aws_security_group_rule" "blog_http_in" {
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type         = "ingress"
-  from_port    = 443
-  to_port      = 443
-  protocol     = "tcp"
-  cidr_blocks  = ["0.0.0.0/0"]
-
+  type                  = "ingress"
+  from_port             = 443
+  to_port               = 443
+  protocol              = "tcp"
+  cidr_blocks           = ["0.0.0.0/0"]
   aws_security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type         = "egress"
-  from_port    = 0
-  to_port      = 0
-  protocol     = "-1"
-  cidr_blocks  = ["0.0.0.0/0"]
-
+  type                  = "egress"
+  from_port             = 0
+  to_port               = 0
+  protocol              = "-1"
+  cidr_blocks           = ["0.0.0.0/0"]
   aws_security_group_id = aws_security_group.blog.id
 }
